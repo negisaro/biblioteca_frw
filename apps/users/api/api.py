7e2e07141532adc61d74b116a438b95a2dcd7ec5
Giova Salazar <nesalaz56@gmail.com>
2023-05-08 20:36:27 -0500

    configuracin user serializable

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,22 +1,133 @@
-from rest_framework.response import Response
+"""from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.views import APIView
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 from rest_framework.decorators import api_view
 
-"""class UserAPIView(APIView):
-    
-    def get(self, request):
-        users = User.objects.all()
-        users_serializer = UserSerializer(users, many=True)
-        return Response(users_serializer.data)"""
 
-
-@api_view(['GET', 'POST'])     
+@api_view(['GET', 'POST'])
 def user_api_view(request):
-    
-    if request.method == 'GET':   
+
+    if request.method == 'GET':
         users = User.object.all()
         users_serializer = UserSerializer(users, many=True)
         return Response(users_serializer.data)
+
+    elif request.method == 'POST':
+        users_serializer = UserSerializer(data=request.data)
+
+        if users_serializer.is_valid():
+            users_serializer.save()
+
+            return Response({'message': 'El usuario fue creado con exito'}, users_serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response({'message': 'El usuario no fue creado'}, users_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_api_view(request, pk=None):
+    user = User.object.filter(id=pk).first()
+    if user:
+        if request.method == 'GET':
+            user_serializer = UserSerializer(user)
+            return Response(user_serializer.data)
+        elif request.method == 'PUT':
+            user_serializer = UserSerializer(user, data=request.data)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                return Response(user_serializer.data)
+            return Response(user_serializer.data)
+        elif request.method == 'DELETE':
+            user.delete()
+            return Response('Eliminado')
+    return Response({'message': f'El usuario con id {user.id} esta creado'})"""
+
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets
+
+from apps.users.models import User
+from apps.users.api.serializers import (
+    UserSerializer, UserListSerializer, UpdateUserSerializer,
+    PasswordSerializer
+)
+
+class UserViewSet(viewsets.GenericViewSet):
+    model = User
+    serializer_class = UserSerializer
+    list_serializer_class = UserListSerializer
+    queryset = None
+
+    def get_object(self, pk):
+        return get_object_or_404(self.model, pk=pk)
+
+    def get_queryset(self):
+        if self.queryset is None:
+            self.queryset = self.model.object\
+                            .filter(is_active=True)\
+                            .values('id', 'username', 'email', 'name')
+        return self.queryset
+
+    @action(detail=True, methods=['post'])
+    def set_password(self, request, pk=None):
+        user = self.get_object(pk)
+        password_serializer = PasswordSerializer(data=request.data)
+        if password_serializer.is_valid():
+            user.set_password(password_serializer.validated_data['password'])
+            user.save()
+            return Response({
+                'message': 'Contraseña actualizada correctamente'
+            })
+        return Response({
+            'message': 'Hay errores en la información enviada',
+            'errors': password_serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def list(self, request):
+        users = self.get_queryset()
+        users_serializer = self.list_serializer_class(users, many=True)
+        return Response(users_serializer.data, status=status.HTTP_200_OK)
     
+    def create(self, request):
+        user_serializer = self.serializer_class(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response({
+                'message': 'Usuario registrado correctamente.'
+            }, status=status.HTTP_201_CREATED)
+        return Response({
+            'message': 'Hay errores en el registro',
+            'errors': user_serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        user = self.get_object(pk)
+        user_serializer = self.serializer_class(user)
+        return Response(user_serializer.data)
+    
+    def update(self, request, pk=None):
+        user = self.get_object(pk)
+        user_serializer = UpdateUserSerializer(user, data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response({
+                'message': 'Usuario actualizado correctamente'
+            }, status=status.HTTP_200_OK)
+        return Response({
+            'message': 'Hay errores en la actualización',
+            'errors': user_serializer.errors
+        }, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        user_destroy = self.model.object.filter(id=pk).update(is_active=False)
+        if user_destroy == 1:
+            return Response({
+                'message': 'Usuario eliminado correctamente'
+            })
+        return Response({
+            'message': 'No existe el usuario que desea eliminar'
+        }, status=status.HTTP_404_NOT_FOUND)
--- biblioteca_frw/urls.py
+++ biblioteca_frw/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path, include
-
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -42,5 +41,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc',
                                        cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
-    path('usuario/', include('apps.users.api.urls')),
+    path('usuario/', include('apps.users.api.routers')),
 ]
